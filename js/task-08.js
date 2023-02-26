4e02e94043114987ae50670857b8f1d6c026c052
Yuliia1710 <taranjuliapetrivna@gmail.com>
2023-02-26 21:45:12 +0200

    added all task

--- js/task-08.js
+++ js/task-08.js
@@ -3,3 +3,29 @@
 // Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElem = event.currentTarget.elements;
+
+  const email = formElem.email.value;
+
+  const password = formElem.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+
+  if (email === "" || password === "") {
+    alert("Все обязательные поля должны быть заполнены!");
+  } else {
+    console.log(formData);
+  }
+  form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,27 @@
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
+const body = document.querySelector("body");
+console.log(body);
+
+const btnEl = document.querySelector(".change-color");
+console.log(btnEl);
+
+const colorVal = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+btnEl.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  let currentColor = getRandomHexColor();
+
+  body.style.backgroundColor = currentColor;
+
+  colorVal.textContent = currentColor;
 }
